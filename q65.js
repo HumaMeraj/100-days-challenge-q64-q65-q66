e1662d3f84514ecb2b527b0f6e775acc82b9a57d
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-04 03:07:04 +0500

    File of q65

---
+++ q65.js
@@ -0,0 +1,7 @@
+//Determine the Remainder: Make a function that gets two numbers and shows the leftover from dividing them using the % sign. For example, remainder(5, 2) should give 1.
+// This function finds the leftover of dividing two numbers
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+// Trying it with 9 divided by 2
+console.log(remainder(9, 2)); //  shows result 1
---
+++ q65.ts
@@ -0,0 +1,12 @@
+//Determine the Remainder: Make a function that gets two numbers and shows the leftover from dividing them using the % sign. For example, remainder(5, 2) should give 1.
+
+
+// This function finds the leftover of dividing two numbers
+function remainder(num1: number, num2: number):number{
+    return num1 % num2
+}
+
+// Trying it with 9 divided by 2
+
+console.log(remainder (9, 2));         //  shows result 1
+
