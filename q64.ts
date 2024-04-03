f382dc8fae88d7f54a46dc27b65e2a9381a5b7a1
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-04 03:06:34 +0500

    File of q64

---
+++ q64.ts
@@ -0,0 +1,11 @@
+//Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
+
+// This function mixes a text and a number into one text
+ // Joins the text and number into a single text
+function combineStringAndNumber(text: string, number: number):string{
+    return text + number;                   
+}
+
+// Trying it out with "Age: " and 30
+
+console.log(combineStringAndNumber("age:", 30));         // Shows "Age: 30"
